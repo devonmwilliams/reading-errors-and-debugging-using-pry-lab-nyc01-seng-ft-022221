c1c74c0d6665eae8eb21cb2eb507f8797937359f
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-14 04:52:01 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
-def snake_it_up(string)
+def snake_it_up("string")
   if string[0] == "s"
-  "s+s+s+s+s+s+s+s+s+s" + string
+  "s+s+s+s+s+s+s+s+s+s" + "string"
   binding.pry
   else
     string
