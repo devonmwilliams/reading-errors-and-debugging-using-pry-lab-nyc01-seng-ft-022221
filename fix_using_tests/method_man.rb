954e5173d8f8d96c950f4a56cf5f427e11b7e1e4
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-14 04:31:35 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,10 +5,10 @@ def start_game(player1, player2)
   player2 = "Bullwinkle"
   "Hello #{player1} & #{player2}"
 end
-binding.pry
 
 def play_game(player1, player2)
   player1 = Jay-Z
   player2 = Beyonce
   "#{player1} is better than #{player2}"
 end
+binding.pry
